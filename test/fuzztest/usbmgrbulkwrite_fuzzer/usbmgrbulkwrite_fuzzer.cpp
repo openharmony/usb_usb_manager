ab7caf85e71bcad1fd3f19d9a7d9df2f000507b3
“wangyipeng” <wangyipeng11@huawei.com>
2022-10-31 14:47:04 +0800

    fixed 19ef497 from https://gitee.com/wang-yi-peng/usb_manager/pulls/134 Fixed the fuzz case compilation failure
    Signed-off-by: “wangyipeng” <wangyipeng11@huawei.com>

--- test/fuzztest/usbmgrbulkwrite_fuzzer/usbmgrbulkwrite_fuzzer.cpp
+++ test/fuzztest/usbmgrbulkwrite_fuzzer/usbmgrbulkwrite_fuzzer.cpp
@@ -32,13 +32,14 @@ namespace USB {
 
         auto& usbSrvClient = UsbSrvClient::GetInstance();
         sptr<UsbCallbackTest> cb = new UsbCallbackTest();
-        auto ret = usbSrvClient.RegBulkCallback(pipe, interface.GetEndpoints().at(1), cb);
+        auto ret = usbSrvClient.RegBulkCallback(
+            const_cast<USBDevicePipe &>(pipe), const_cast<UsbInterface &>(interface).GetEndpoints().at(1), cb);
         if (ret != UEC_OK) {
             USB_HILOGE(MODULE_USB_SERVICE, "RegBulkCallback failed ret=%{public}d", ret);
             return false;
         }
 
-        if (usbSrvClient.BulkWrite(reinterpret_cast<const USBDevicePipe&>(data),
+        if (usbSrvClient.BulkWrite(reinterpret_cast<USBDevicePipe &>(data),
             reinterpret_cast<const USBEndpoint&>(data), reinterpret_cast<sptr<Ashmem>&>(data)) == UEC_OK) {
             return false;
         }
