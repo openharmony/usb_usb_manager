fd61a48cbadaa59b43a6ccee52611b0535195011
luo-wei246 <luowei137@huawei.com>
2023-02-16 14:50:03 +0800

    fix:The argument type returns an error
    Signed-off-by: luo-wei246 <luowei137@huawei.com>

--- interfaces/kits/js/napi/src/napi_util.cpp
+++ interfaces/kits/js/napi/src/napi_util.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -147,6 +147,8 @@ bool NapiUtil::JsUint8ArrayParse(
 {
     bool isTypedArray = false;
     if (napi_is_typedarray(env, object, &isTypedArray) != napi_ok || !isTypedArray) {
+        USB_ASSERT_RETURN_FALSE(
+           env, isTypedArray, SYSPARAM_INVALID_INPUT, "The type of buffer must be TypedArray.");
         USB_HILOGW(MODULE_JS_NAPI, "invalid type");
         return false;
     }
@@ -160,17 +162,10 @@ bool NapiUtil::JsUint8ArrayParse(
         USB_HILOGW(MODULE_JS_NAPI, "get typedarray info failed, status: %{public}d", infoStatus);
         return false;
     }
-
-    if (type != napi_uint8_array) {
-        USB_HILOGW(MODULE_JS_NAPI, "not Uint8Array type: %{public}d", type);
-        return false;
-    }
-
-    if (bufferSize == 0) {
-        USB_HILOGW(MODULE_JS_NAPI, "bufferSize error");
-        return false;
-    }
-
+    USB_ASSERT_RETURN_FALSE(
+        env, type == napi_uint8_array, SYSPARAM_INVALID_INPUT, "The type of buffer must be Uint8Array.");
+    USB_ASSERT_RETURN_FALSE(
+        env, bufferSize != 0, SYSPARAM_INVALID_INPUT, "The size of buffer must be greater than zero.");
     return true;
 }
 
