91d1826f6d94f01d1b4519e3a8f983b3b8c0f643
wangyipeng <wangyipeng11@huawei.com>
2023-04-06 16:04:46 +0800

    Fix:Broadcast events require no delay
    Signed-off-by: wangyipeng <wangyipeng11@huawei.com>

--- services/native/src/usb_device_manager.cpp
+++ services/native/src/usb_device_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -164,10 +164,8 @@ void UsbDeviceManager::HandleEvent(int32_t status)
 
     CommonEventData data(want);
     CommonEventPublishInfo publishInfo;
-    publishInfo.SetOrdered(true);
-    USB_HILOGI(MODULE_SERVICE,
-        "send COMMON_EVENT_USB_STATE broadcast connected:%{public}d, currentFunctions:%{public}u", connected_,
-        currentFunctions_);
+    USB_HILOGI(MODULE_SERVICE, "send COMMON_EVENT_USB_STATE broadcast connected:%{public}d, "
+        "currentFunctions:%{public}u", connected_, currentFunctions_);
     CommonEventManager::PublishCommonEvent(data, publishInfo);
     ReportDevicePlugSysEvent(currentFunctions_, connected_);
 }
--- services/native/src/usb_service_subscriber.cpp
+++ services/native/src/usb_service_subscriber.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -58,7 +58,6 @@ int32_t UsbServiceSubscriber::PortChangedEvent(const PortInfo &info)
     data.SetData(jsonString);
     data.SetWant(want);
     CommonEventPublishInfo publishInfo;
-    publishInfo.SetOrdered(true);
     bool isSuccess = CommonEventManager::PublishCommonEvent(data, publishInfo);
     if (!isSuccess) {
         USB_HILOGE(MODULE_USB_SERVICE, "failed to publish PortChangedEvent");
