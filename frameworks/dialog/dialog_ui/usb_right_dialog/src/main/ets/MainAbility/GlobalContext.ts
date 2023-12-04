357bed8ec4168d41f5eff8a1c7ab6243799a3a8b
zwx1283032 <zhouyi167@huawei.com>
2023-12-04 13:09:24 +0000

    ets代码告警处理
    Signed-off-by: zwx1283032 <zhouyi167@huawei.com>

--- frameworks/dialog/dialog_ui/usb_right_dialog/src/main/ets/MainAbility/GlobalContext.ts
+++ frameworks/dialog/dialog_ui/usb_right_dialog/src/main/ets/MainAbility/GlobalContext.ts
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import convertContext from "./List.ts"
+import convertContext from './List.ts';
 
 // 构造单例对象
 export class GlobalContext {
