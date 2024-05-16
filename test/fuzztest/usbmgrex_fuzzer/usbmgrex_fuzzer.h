26a06d8a8fc4a67caced79db732e80af2b5c61af
openharmony_ci <120357966@qq.com>
2024-05-16 01:02:15 +0000

    回退 'Pull Request !430 : 回退 'Pull Request !429 : 【修复】FUZZ 测试用例增加''

---
+++ test/fuzztest/usbmgrex_fuzzer/usbmgrex_fuzzer.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_USBMGREX_FUZZER_H
+#define OHOS_USBMGREX_FUZZER_H
+#include "accesstoken_kit.h"
+#include "hdf_log.h"
+#include "nativetoken_kit.h"
+#include "token_setproc.h"
+#include "usb_common_test.h"
+#include "usb_errors.h"
+#include "hilog_wrapper.h"
+
+#define FUZZ_PROJECT_NAME "usbmgrex_fuzzer"
+
+#endif // OHOS_USBMGREX_FUZZER_H
