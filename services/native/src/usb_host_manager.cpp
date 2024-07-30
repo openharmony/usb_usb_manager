dfb754a27c6f52412fee473e8ef93aebbe0984d2
luzhiye <luzhiye123@huawei.com>
2024-07-30 07:17:09 +0000

    update services/native/src/usb_host_manager.cpp.
    Signed-off-by: luzhiye <luzhiye123@huawei.com>

--- services/native/src/usb_host_manager.cpp
+++ services/native/src/usb_host_manager.cpp
@@ -116,7 +116,7 @@ bool UsbHostManager::AddDevice(UsbDevice *dev)
         USB_HILOGF(MODULE_SERVICE, "device:%{public}s bus:%{public}hhu dev:%{public}hhu already exist", name.c_str(),
             busNum, devNum);
         UsbDevice *devOld = iter->second;
-        if (devOld != nullptr) {
+        if (devOld != nullptr && busNum == devOld->GetBusNum() && devNum == devOld->GetDevAddr()) {
             delete devOld;
         }
         devices_.erase(iter);
