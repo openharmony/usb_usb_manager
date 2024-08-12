f68b20f0b78760efdef47edd2073b376ad0dce0b
luzhiye <luzhiye123@huawei.com>
2024-08-12 06:29:01 +0000

    空指针问题整改
    Signed-off-by: luzhiye <luzhiye123@huawei.com>

--- services/native/src/usbd_bulkcallback_impl.cpp
+++ services/native/src/usbd_bulkcallback_impl.cpp
@@ -36,6 +36,10 @@ int32_t UsbdBulkCallbackImpl::OnBulkWriteCallback(int32_t status, int32_t actLen
         USB_HILOGE(MODULE_USB_SERVICE, "%{public}s: write actLength failed", __func__);
         return UEC_SERVICE_INVALID_VALUE;
     }
+    if (remote_ == nullptr) {
+        USB_HILOGE(MODULE_USB_SERVICE, "%{public}s: remote_ is nullptr", __func__);
+        return UEC_SERVICE_INVALID_VALUE;
+    }
     int32_t ret = remote_->SendRequest(UsbdBulkCallBack::CMD_USBD_BULK_CALLBACK_WRITE, data, reply, option);
     if (ret != UEC_OK) {
         USB_HILOGE(MODULE_USB_SERVICE, "%{public}s failed, error code is %{public}d", __func__, ret);
@@ -51,12 +55,14 @@ int32_t UsbdBulkCallbackImpl::OnBulkReadCallback(int32_t status, int32_t actLeng
         USB_HILOGE(MODULE_USB_SERVICE, "%{public}s: write status failed", __func__);
         return UEC_SERVICE_INVALID_VALUE;
     }
-
     if (!data.WriteInt32(actLength)) {
         USB_HILOGE(MODULE_USB_SERVICE, "%{public}s: write actLength failed", __func__);
         return UEC_SERVICE_INVALID_VALUE;
     }
-
+    if (remote_ == nullptr) {
+        USB_HILOGE(MODULE_USB_SERVICE, "%{public}s: remote_ is nullptr", __func__);
+        return UEC_SERVICE_INVALID_VALUE;
+    }
     OHOS::MessageParcel reply;
     OHOS::MessageOption option;
     int32_t ret = remote_->SendRequest(UsbdBulkCallBack::CMD_USBD_BULK_CALLBACK_READ, data, reply, option);
