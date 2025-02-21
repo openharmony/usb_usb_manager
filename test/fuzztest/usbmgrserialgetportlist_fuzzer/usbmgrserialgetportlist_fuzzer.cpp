dd8d1e9e894e5e363c2bbcd56aeede7302cb060f
dairan <dairan1@h-partners.com>
2025-02-21 21:21:40 +0800

    添加fuzz用例
    Signed-off-by: dairan <dairan1@h-partners.com>

---
+++ test/fuzztest/usbmgrserialgetportlist_fuzzer/usbmgrserialgetportlist_fuzzer.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "usbmgrserialgetportlist_fuzzer.h"
+
+#include <vector>
+#include "usb_srv_client.h"
+
+namespace {
+constexpr int32_t OK = 0;
+}
+using OHOS::USB::UsbSrvClient;
+namespace OHOS {
+namespace SERIAL {
+bool UsbMgrSerialGetPortListFuzzTest(const uint8_t* data, size_t size)
+{
+    auto &usbSrvClient = UsbSrvClient::GetInstance();
+    std::vector<OHOS::HDI::Usb::Serial::V1_0::SerialPort> portInfo;
+    if (usbSrvClient.SerialGetPortList(portInfo) != OK || portInfo.size() == 0) {
+        return false;
+    }
+
+    return true;
+}
+} // SERIAL
+} // OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::SERIAL::UsbMgrSerialGetPortListFuzzTest(data, size);
+    return 0;
+}
+
