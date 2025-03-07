c8c1fa7a53b505c68e0cd04c6ca4d7dfe31f25c3
huangshengqing <huangshengqing8@huawei.com>
2025-03-07 03:37:35 +0000

    update services/native/src/usb_service.cpp.
    Signed-off-by: huangshengqing <huangshengqing8@huawei.com>

--- services/native/src/usb_service.cpp
+++ services/native/src/usb_service.cpp
@@ -725,7 +725,7 @@ bool UsbService::DelDevice(uint8_t busNum, uint8_t devAddr)
         }
     }
     {
-        std::unique_lock<std::shared_mutex> lock(openedFdsMutex_);
+        std::lock_guard<std::shared_mutex> lock(openedFdsMutex_);
         auto iter = openedFds_.find({busNum, devAddr});
         if (iter != openedFds_.end()) {
             int32_t fd = iter->second;
@@ -833,7 +833,7 @@ int32_t UsbService::GetFileDescriptor(uint8_t busNum, uint8_t devAddr, int32_t &
     if (ret != UEC_OK) {
         USB_HILOGE(MODULE_USB_SERVICE, "get fd error ret:%{public}d", ret);
     } else {
-        std::shared_lock<std::shared_mutex> lock(openedFdsMutex_);
+        std::lock_guard<std::shared_mutex> lock(openedFdsMutex_);
         auto iter = openedFds_.find({busNum, devAddr});
         if (iter != openedFds_.end()) {
             int32_t oldFd = iter->second;
