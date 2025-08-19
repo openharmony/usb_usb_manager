ca9ee2dbd93fb6b0f6e12048fbf2714ba5b0ec87
fengliucs <liufeng243@h-partners.com>
2025-08-19 11:29:59 +0800

    同步fuzz用例
    Signed-off-by: fengliucs <liufeng243@h-partners.com>

--- test/fuzztest/usbmgrserialread_fuzzer/usbmgrserialread_fuzzer.cpp
+++ test/fuzztest/usbmgrserialread_fuzzer/usbmgrserialread_fuzzer.cpp
@@ -29,7 +29,7 @@ namespace USB {
         uint32_t bufferSize = 0;
         uint32_t actualSize = 0;
         uint32_t timeout = 0;
-        std::vector<uint8_t> = buf;
+        std::vector<uint8_t> buf;
         int32_t ret = usbSrvClient.SerialRead(*reinterpret_cast<const int32_t*>(data), buf,
             bufferSize, actualSize, timeout);
         if (ret == UEC_OK) {
