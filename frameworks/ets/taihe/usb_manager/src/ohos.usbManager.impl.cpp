15a34f9eb59b28abb46be3c5fe7767cc4df0c270
LiuFeng_Again <liufeng243@h-partners.com>
2026-02-10 10:43:31 +0800

    update: 更新文件 ohos.usbManager.impl.cpp
    Signed-off-by: LiuFeng_Again <liufeng243@h-partners.com>

--- frameworks/ets/taihe/usb_manager/src/ohos.usbManager.impl.cpp
+++ frameworks/ets/taihe/usb_manager/src/ohos.usbManager.impl.cpp
@@ -1183,6 +1183,7 @@ static void AniCallBack(USBTransferAsyncContext *asyncContext, const OHOS::USB::
         auto errCode = env->FunctionalObject_Call(static_cast<ani_fn_object>(callbackFunc), 2, ani_argv, &ani_result);
         USB_HILOGE(MODULE_USB_NAPI, "AniCallBack FunctionalObject_Call returned %{public}d.", errCode);
         env->DestroyLocalScope();
+        DeleteCallback(asyncContext);
     };
     if (!SendEventToMainThread(task)) {
         USB_HILOGI(MODULE_USB_NAPI, "SendEventToMainThread failed.");
